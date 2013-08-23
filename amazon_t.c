0ba679e9042d64d067646ec0bc67de4f0106327e
Harsh Rawat <hrawat0022@gmail.com>
2013-08-23 17:01:26 +0530

    trial start

---
+++ amazon_t.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+struct stack
+{
+    int top;
+    char info[100];
+};
+void cal(char []);
+void push(struct stack *, char);
+char pop(struct stack *);
+main()
+{
+    char in[100];
+    int i;
+    for(i=0;(in[i]=getchar())!='\n';i++)
+    {
+
+    }
+    in[i]='\0';
+    cal(in);
+    return(0);
+}
+void cal(char in[100])
+{
+    int i,t=0;
+    char c,k;
+    struct stack s;
+    s.top=-1;
+    for(i=0;(c=in[i])!='\0';i++)
+    {
+        if(c=='(' || c=='{' || c=='[')
+           push(&s,c);
+        if(c==')')
+        {
+            k=pop(&s);
+            if(k!='(')
+            {
+                printf("FALSE ( \n");
+                t++;
+                break;
+            }
+        }
+        if(c=='}')
+        {
+            k=pop(&s);
+            if(k!='{')
+            {
+                printf("FALSE { \n");
+                t++;
+                break;
+            }
+        }
+        if(c==']')
+        {
+            k=pop(&s);
+            if(k!='[')
+            {
+                printf("FALSE ]\n");
+                t++;
+                break;
+            }
+        }
+    }
+    if(t==0)
+    printf("True\n");
+}
+void push(struct stack *ps, char c)
+{
+    ps->info[++(ps->top)]=c;
+    return;
+}
+char pop(struct stack *ps)
+{
+    return(ps->info[(ps->top)--]);
+}
+
+
+
+
+
+
+
+
+
